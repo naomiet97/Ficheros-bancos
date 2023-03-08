265d785bd2352c50d68f439b69c7f8861848e62d
naomiet97 <122862721+naomiet97@users.noreply.github.com>
2023-03-08 23:55:33 +0100

    adding Cuenta.java, CuentaCaixa.java, CuantaSabadell.java, CuentaSantander.java

---
+++ src/main/java/entidades/CuentaCaixa.java
@@ -0,0 +1,10 @@
+package entidades;
+
+public class CuentaCaixa extends Cuenta {
+	
+	
+    public CuentaCaixa(String dni, String name, String birthDate,
+            String postalCode, double balance, String type) {
+        super(dni, name, birthDate, postalCode, balance, type);
+    }
+}
---
+++ src/main/java/entidades/CuentaSabadell.java
@@ -0,0 +1,10 @@
+package entidades;
+
+public class CuentaSabadell extends Cuenta {
+
+    public CuentaSabadell(String dni, String name, String birthDate,
+            String postalCode, double balance, String type) {
+        super(dni, name, birthDate, postalCode, balance, type);
+    }
+}
+
